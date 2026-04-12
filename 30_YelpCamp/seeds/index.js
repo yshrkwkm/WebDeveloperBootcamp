5d3795e50110fdde72232c994ed92718b314eeb5
yshrkwkm <j4ngl1sh@gmail.com>
2026-04-12 23:42:10 +0900

    Add Yelp Camp first code

---
+++ 30_YelpCamp/seeds/index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', 
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('Mongo DB 接続OK！！！');
+    })
+    .catch(err => {
+        console.log('Mongo DB コネクションエラー！！！');
+        console.log(err);
+    });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+            title: `${sample(descriptors)}・${sample(places)}`
+        });
+        await camp.save();
+    }
+} 
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
