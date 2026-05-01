a7db5f251c9deef4802bf4a8fcda2d59ce65ff5a
yshrkwkm <j4ngl1sh@gmail.com>
2026-05-01 12:26:47 +0900

    Add auth demo

---
+++ 41_AUTHDEMO/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const user = require('./models/user');
+const session = require('express-session');
+
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo', 
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => {
+        console.log('Mongo DB コネクションOK！！！');
+    })
+    .catch(err => {
+        console.log('Mongo DB コネクションエラー！！！');
+        console.log(err);
+    });
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'mysecret'}));
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+};
+
+app.get('/', (req, res) => {
+    res.send('ホームページ');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const user = new User({
+        username,
+        password
+    });
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if (validPassword) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.post('/logout', (req, res) => {
+    // req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('TOP SECRET!');
+});
+
+app.listen(3000, () => {
+    console.log('ポート3000で待受中...');
+});
---
+++ 41_AUTHDEMO/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'usernameは必須です']
+    },
+    password: {
+        type: String,
+        required: [true, 'passwordは必須です']
+    }
+});
+
+userSchema.statics.findAndValidate = async function(username, password) {
+    const foundUser = await this.findOne({username});
+    const isValid = bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
