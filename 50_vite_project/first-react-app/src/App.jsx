64f0141fc1ccaca05d3688bad0f2003af5901298
yshrkwkm <j4ngl1sh@gmail.com>
2026-05-19 00:04:53 +0900

    Add local React development with Vite

---
+++ 50_vite_project/first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Chicken from './Chicken';
+
+function App() {
+  return (
+    <div>
+      <Chicken />
+      <Chicken />
+      <Chicken />
+    </div>
+  )
+}
+
+export default App
---
+++ 50_vite_project/first-react-app/src/Chicken.jsx
@@ -0,0 +1,7 @@
+import './Chicken.css'
+
+function Chicken() {
+    return <p className='Chicken'>コケコッコー</p>
+}
+
+export default Chicken;
