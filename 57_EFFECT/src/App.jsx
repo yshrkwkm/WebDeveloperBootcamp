b7d7acdc3ba1aa7539c5d396318b19887e6ad8ab
yshrkwkm <j4ngl1sh@gmail.com>
2026-06-17 14:43:47 +0900

    Add useEffect

---
+++ 57_EFFECT/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import QuoteFetcherLoader from './QuoteFetcherLoader'
+
+function App() {
+
+  return (
+    <>
+      <QuoteFetcherLoader />
+    </>
+  )
+}
+
+export default App
---
+++ 57_EFFECT/src/Counter.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    useEffect(
+        function myEffect() {
+            console.log('エフェクトが呼ばれたよ');
+        }, 
+        [count]
+    );
+    const increment = () => {
+        setCount((c) => c + 1);
+    };
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+1</button>
+            <p>{name}</p>
+            <input value={name} onChange={handleChange} type="text" />
+        </div>
+    );
+}
